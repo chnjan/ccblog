3ab3a0a1ea5d6c53830ab1593c604f48d81013c4
chnjan <30980930+chnjan@users.noreply.github.com>
2017-12-14 23:11:45 +0800

    20171214

---
+++ src/main/java/com/chnjan/ccblog/main/domain/Blog.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.chnjan.ccblog.main.domain;
+
+import java.util.Date;
+
+/**
+ * 2017-12-14
+ * @author chenjian
+ * 博客信息实体类
+ */
+public class Blog {
+	//id
+	private String blogId;
+	//创建时间
+	private Date createTime;
+	//最近更新时间
+	private Date lastUpdateTime;
+	//标题
+	private String title;
+	//内容
+	private String content;
+	//作者id
+	private String autorid;
+	
+	
+	public String getBlogId() {
+		return blogId;
+	}
+	public void setBlogId(String blogId) {
+		this.blogId = blogId;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getLastUpdateTime() {
+		return lastUpdateTime;
+	}
+	public void setLastUpdateTime(Date lastUpdateTime) {
+		this.lastUpdateTime = lastUpdateTime;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getAutorid() {
+		return autorid;
+	}
+	public void setAutorid(String autorid) {
+		this.autorid = autorid;
+	}
+	
+	@Override
+	public String toString() {
+		return "Blog [blogId=" + blogId + ", createTime=" + createTime + ", lastUpdateTime=" + lastUpdateTime
+				+ ", title=" + title + ", content=" + content + ", autorid=" + autorid + "]";
+	}
+
+}
